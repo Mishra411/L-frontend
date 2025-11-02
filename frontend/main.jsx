2089556c4fd00c6da19d58c136b65ea8daeea0db
Aalok Mishra <mishraaalok75@gmail.com>
2025-11-01 18:28:10 -0600

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Dashboard from "./Pages/Dashboard";
-import Analytics from "./Pages/Analytics";
-import ReportDetails from "./Pages/ReportDetails";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/analytics" element={<Analytics />} />
-        <Route path="/report/:id" element={<ReportDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Analytics from './pages/Analytics.jsx';
+import ReportDetails from './pages/ReportDetails.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/report/:id" element={<ReportDetails />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </App>
+    </BrowserRouter>
   </React.StrictMode>
 );
