0f539fe5169f32ec28c1b08eacc9f3902f14f35b
Aalok Mishra <mishraaalok75@gmail.com>
2025-11-01 18:29:43 -0600

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Analytics from './pages/Analytics.jsx';
@@ -12,15 +11,13 @@ import ReportDetails from './pages/ReportDetails.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/report/:id" element={<ReportDetails />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </App>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/report/:id" element={<ReportDetails />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
