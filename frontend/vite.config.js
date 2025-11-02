ace082e7b29b4d694df67e5645d3ff3cd2fccd3e
Aalok Mishra <mishraaalok75@gmail.com>
2025-11-01 20:32:04 -0600

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // matches what server.js serves
+    outDir: "dist", // where production build is output
+  },
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./frontend"),
-           },
-  }
-})
+    },
+  },
+});
