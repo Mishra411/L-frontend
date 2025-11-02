4081a97e152ecd67f2ae958b71741749b7aca5ea
Aalok Mishra <mishraaalok75@gmail.com>
2025-11-01 20:29:34 -0600

    Update Dashboard.jsx

--- frontend/Pages/Dashboard.jsx
+++ frontend/Pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { getAllReports } from "@/api/reportApi";
+import { getAllReports } from "@/reportApi";
 
 import ReportForm from "@/Components/reports/ReportForm";
 import ReportFilters from "@/Components/reports/ReportFilters";
