339e3aa9a41f935eca80c2ef0e23ee68817db3d4
Aalok Mishra <mishraaalok75@gmail.com>
2025-11-01 20:41:04 -0600

    Update Analytics.jsx

--- frontend/Pages/Analytics.jsx
+++ frontend/Pages/Analytics.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import { getStats } from "@/reportApi";
+import { getStats } from "@/reportApi.js";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/Components/ui/card";
 import { Skeleton } from "@/Components/ui/skeleton";
 import {
