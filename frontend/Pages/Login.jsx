5c268df4f948cb3d1c0991246e5365cfdc12a977
Aalok Mishra <mishraaalok75@gmail.com>
2025-11-01 20:47:29 -0600

    Update Login.jsx

--- frontend/Pages/Login.jsx
+++ frontend/Pages/Login.jsx
@@ -1,6 +1,6 @@
 // src/pages/Login.jsx
 import React, { useState } from "react";
-import { Button } from "@/Components/ui/button";
+import { Button } from "@/Components/ui/Button.jsx";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
